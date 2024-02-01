7344722a75eec3f13c22b377170d8257a2b976f1
rizash <sajmerdenr@gmail.com>
2024-02-01 07:15:46 +0600

    finished styling the first page, added download button

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,17 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+)
 
 func (app *application) RegisterRoutes() {
 	SrvMux := http.NewServeMux()
-	SrvMux.HandleFunc("/", app.Home)
+	SrvMux.HandleFunc("/", app.home)
+	SrvMux.HandleFunc("/download/", app.download)
 
 	SrvMux.Handle("/static/", http.StripPrefix("/static", http.FileServer(NeuteredFileSystem{http.Dir("./ui/static/")})))
 
 	app.SrvMux = SrvMux
 }
 
-func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	switch {
 	case r.URL.Path != "/":
 		app.notFound(w)
@@ -19,4 +27,37 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.render(w, r, "home.page.html")
+}
+
+func (app *application) download(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	f, err := os.Open("./ui/static/img/Resume_template.pdf")
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	defer f.Close()
+
+	contentDisposition := fmt.Sprintf("attachment; filename=%s", path.Base(f.Name()))
+	w.Header().Set("Content-Disposition", contentDisposition)
+
+	contentType := "text/plain; charset=utf-8"
+	w.Header().Set("Content-Type", contentType)
+
+	finfo, err := f.Stat()
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	contentLength := strconv.Itoa(int(finfo.Size()))
+	w.Header().Set("Content-Length", contentLength)
+
+	if _, err := io.Copy(w, f); err != nil {
+		app.serverError(w, err)
+		return
+	}
 }
